8324129babe33f67b8ecbcb1e9b6e8244c3cbca0
Blimax25 <byronlimaster@gmail.com>
2024-05-29 22:01:37 -0500

    Formato cadenas

---
+++ 07-FORMATO_CADENAS/Practica1.py
@@ -0,0 +1,11 @@
+"""
+PRACTICA 1
+Necesitamos imprimir el nombre y número de 
+asociado dentro de la siguiente frase: 
+Estimado/a (nombre_asociado), su número de asociado es: (numero_asociado)
+Utilizando el primer metodo
+"""
+nombre="Byron"
+numeroAsociado=30;
+
+print("Estimado/a {}, su número de asociado es: {}".format(nombre,numeroAsociado))
---
+++ 07-FORMATO_CADENAS/app.py
@@ -0,0 +1,5 @@
+x=10
+y=5
+z=x+y
+#primera forma de convertir cadenas
+print("La suma de {} y {} es igual a {}".format(x,y,z))

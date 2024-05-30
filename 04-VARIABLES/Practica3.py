5230b9cb5c2cb496a43089e9ec0b64d2310e831d
Blimax25 <byronlimaster@gmail.com>
2024-05-29 20:02:21 -0500

    Manejo de variables

---
+++ 04-VARIABLES/Practica3.py
@@ -0,0 +1,7 @@
+"""
+PRACTICA 3
+Declara la variable curso, asígnale el valor "Python", 
+y muestra en pantalla la frase:
+"""
+curso = "Python"
+print("En el curso de " + curso + " se aprenderá muchos comandos y funciones.")
---
+++ 04-VARIABLES/app.py
@@ -0,0 +1,14 @@
+nombre = "Juan"
+#print(nombre)
+nombre = "Laura"
+nombre = "Jose"
+print(nombre)
+
+xx = "Tomate"
+print(xx)
+
+edad1=30
+edad2=60
+
+edadTotal=edad1+edad2
+print(f"Edad Total: {edadTotal}")

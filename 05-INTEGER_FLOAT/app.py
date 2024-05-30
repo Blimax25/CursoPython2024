893b60402c2585240eba63fcab9658529a37a551
Blimax25 <byronlimaster@gmail.com>
2024-05-29 20:56:07 -0500

    Variables Enteras y Flotantes

---
+++ 05-INTEGER_FLOAT/app.py
@@ -0,0 +1,13 @@
+mi_numero="1"
+mi_numero2=5.8
+#print(mi_numero)
+
+#verificar el tipo de dato
+#print(type(mi_numero2))
+
+#ejemplo
+edad=input("Dime tu edad: ")
+print("Tu edad es: " + edad)
+
+nuevaEdad = 1 + edad
+print(nuevaEdad)

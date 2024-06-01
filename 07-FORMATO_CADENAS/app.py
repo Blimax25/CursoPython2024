aafb3d7a907ce38740994adc87f00caeb7b2cde7
Blimax25 <byronlimaster@gmail.com>
2024-05-31 19:55:57 -0500

    Listas

--- 07-FORMATO_CADENAS/app.py
+++ 07-FORMATO_CADENAS/app.py
@@ -3,3 +3,8 @@ y=5
 z=x+y
 #primera forma de convertir cadenas
 print("La suma de {} y {} es igual a {}".format(x,y,z))
+
+#actualizacion nuevas versiones de python
+color = "rojo"
+matricula = 541923
+print(f"El auto es {color} y su matricula es {matricula}") #formato de cadenas
---
+++ 13-LISTAS/Practica1.py
@@ -0,0 +1,8 @@
+"""
+Practica 1
+Crea una lista con 5 elementos, dentro de 
+la variable mi_lista. Puedes incluir strings, booleanos, números, etc
+"""
+mi_lista = [True,"Casa",3000,False,"Garage"]
+print(type(mi_lista))
+print(mi_lista)

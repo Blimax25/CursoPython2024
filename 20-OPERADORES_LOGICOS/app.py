fd278c5f5f1fb113edb06d382d78effffd41d464
Blimax25 <byronlimaster@gmail.com>
2024-06-04 19:54:29 -0500

    practicas python

---
+++ 20-OPERADORES_LOGICOS/app.py
@@ -0,0 +1,17 @@
+mi_bool = 4 < 5 < 5
+
+#Ejercicio
+mi_bool = 4 < 5 and 5 > 6
+#and
+mi_bool = (55 == 55) and ('perro' == 'perro')
+print(mi_bool)
+#or
+mi_bool = 10 == 10 or 3 == 7
+print(mi_bool)
+#frase
+texto="esta frase es breve"
+mi_bool = ('frase' in texto) and ('python' in texto)
+print(mi_bool)
+#diferente
+mi_bool = not('a'!='a')
+print(mi_bool)
---
+++ 21-CONTROL_FLUJO/app.py
@@ -0,0 +1,8 @@
+mascota = 'gato'
+if mascota == 'gato':
+    print('Tiene un gato')
+elif mascota == 'perro':
+    print('Tiene un perro')
+else:
+    print('No tiene un animal')
+
